df4e5a7b9c5dda4872fe767951c030fd1ba75581
Juliano <juliano.leite@b2.adm.br>
2016-03-07 21:59:46 -0300

    correção de nomes

--- src/app/modules/index.module.js
+++ src/app/modules/index.module.js
@@ -4,21 +4,21 @@ import CondoUserModule from './condoUser/index.module';
 import HomeModule from './home/index.module';
 import NotificationModule from './notification/index.module';
 import UserModule from './user/index.module';
-import CondoEntityModule from './CondoEntity/index.module';
+import CondoEntityModule from './condoEntity/index.module';
 import CondoAttachmentModule from './CondoAttachment/index.module';
-import AuthorityModule from './AuthorityModule/index.module';
-import CommmentModule from './CommmentModule/index.module';
-import CommentLikerModule from './CommentLikerModule/index.module';
-import CondoAdminModule from './CondoAdminModule/index.module';
-import CustomAuditEventModule from './CustomAuditEventModule/index.module';
-import CustomJpaModule from './CustomJpaModule/index.module';
-import HouseModule from './CustomJpaModule/index.module';
-import HouseResidentModule from './HouseResidentModule/index.module';
-import OccurrenceModule from './OccurrenceModule/index.module';
-import OccurrenceAgainstVoteModule from './OccurrenceAgainstVoteModule/index.module';
-import OccurrenceFavorVoteModule from './OccurrenceFavorVoteModule/index.module';
-import OccurrenceLikerModule from './OccurrenceLikerModule/index.module';
-import PersistenceAuditEventModule from './PersistenceAuditEventModule/index.module';
+import AuthorityModule from './authority/index.module';
+import CommentModule from './comment/index.module';
+import CommentLikerModule from './commentLiker/index.module';
+import CondoAdminModule from './condoAdmin/index.module';
+import CustomAuditEventModule from './customAuditEvent/index.module';
+import CustomJpaModule from './customJpa/index.module';
+import HouseModule from './customJpa/index.module';
+import HouseResidentModule from './houseResident/index.module';
+import OccurrenceModule from './occurrence/index.module';
+import OccurrenceAgainstVoteModule from './occurrenceAgainstVote/index.module';
+import OccurrenceFavorVoteModule from './occurrenceFavorVote/index.module';
+import OccurrenceLikerModule from './occurrenceLiker/index.module';
+import PersistenceAuditEventModule from './persistenceAuditEvent/index.module';
 
 
 export default angular.module('econdos.modules', [
