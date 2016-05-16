8cc6fbc3780ad1b952df22e47a654bd88cf576b3
Ricardo Paiva <ricardo@ricardopaiva.com>
2016-05-16 15:09:04 -0300

    Publish to github pages

--- gulp/publish.js
+++ gulp/publish.js
@@ -7,15 +7,25 @@ var conf = require('./conf');
 var s3 = require('gulp-s3');
 
 var nconf = require('nconf');
+var ghPages = require('gulp-gh-pages');
 
-gulp.task('publish', ['bower', 'build'], function () {
-  var awsProperties = {
-    'key'    : conf.environment.AWS_KEY,
-    'secret' : conf.environment.AWS_SECRET,
-    'bucket' : conf.environment.AWS_BUCKET,
-    'region' : conf.environment.AWS_REGION
-  };
 
-  return gulp.src(path.join(conf.paths.dist, '/**'))
-    .pipe(s3(awsProperties));
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({
+      remoteUrl: 'git@github.com:econdos/frontend.git',
+      force: true
+    }));
 });
+
+// gulp.task('publish', ['bower', 'build'], function () {
+//   var awsProperties = {
+//     'key'    : conf.environment.AWS_KEY,
+//     'secret' : conf.environment.AWS_SECRET,
+//     'bucket' : conf.environment.AWS_BUCKET,
+//     'region' : conf.environment.AWS_REGION
+//   };
+
+//   return gulp.src(path.join(conf.paths.dist, '/**'))
+//     .pipe(s3(awsProperties));
+// });
