406bc202c7b410134e6261f83ed4555d848c25d5
Ricardo Paiva <ricardo@ricardopaiva.com>
2016-08-12 20:27:29 -0300

    Fix refresh

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,4 @@
-export let RunBlock = ($timeout, Session) => {
+export let RunBlock = ($timeout, Session, $state, $stateParams) => { //Bug do ui-router https://github.com/angular-ui/ui-router/issues/105
   'ngInject';
   Session.create();
 };
