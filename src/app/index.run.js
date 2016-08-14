dcfd18933aef4a4f3f99e43356c8018e3749ee91
Ricardo Paiva <ricardo@ricardopaiva.com>
2016-08-14 12:56:27 -0300

    Update bug comments

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,6 @@
-export let RunBlock = ($timeout, Session, $state, $stateParams) => { //Bug do ui-router https://github.com/angular-ui/ui-router/issues/105
+// Bug do ui-router https://github.com/angular-ui/ui-router/issues/105
+// deve-se manter $state, $stateParams
+export let RunBlock = ($timeout, Session, $state, $stateParams) => {
   'ngInject';
   Session.create();
 };
