c6cace554dd9c3b28fd9bb8160c0afac59e37221
Ryan Aghdam <ryan@ryanaghdam.com>
2018-12-22 15:51:47 -0500

    Why did I have an artifical delay?

--- src/adapter.js
+++ src/adapter.js
@@ -1,13 +1,7 @@
 import Axios from "axios";
 import { parseDate } from "./domain.js";
 
-const createDelay = delayInMilliseconds => x =>
-  new Promise(resolve => {
-    setTimeout(() => resolve(x), delayInMilliseconds);
-  });
-
 export const fetchMfuStatistic = () =>
   Axios.get(`/mfu.json?${new Date().toISOString()}`)
     .then(response => response.data)
-    .then(createDelay(700))
     .then(parseDate);
