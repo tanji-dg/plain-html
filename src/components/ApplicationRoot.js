9e804b7d3e05dad011a4fad06143db497c4d9935
raghdam <raghdam@cargurus.com>
2019-02-27 13:29:07 -0500

    The Wall

--- src/components/ApplicationRoot.js
+++ src/components/ApplicationRoot.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import {Provider} from 'react-redux';
 
-import Display from "./Display.js";
-import store from "../store.js";
-import styles from "./ApplicationRoot.css";
+import Display from './Display.js';
+import store from '../store.js';
+import styles from './ApplicationRoot.css';
 
 export default () => (
   <div className={styles.applicationRoot}>
     <Provider store={store}>
       <Display />
+      <div className={styles.wall}>
+        <img src="/404-mifi_vs_the_wall.jpg" />
+      </div>
     </Provider>
   </div>
 );
