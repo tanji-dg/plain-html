136ffaaa5cadf45cd51c125867044c3a48ae9bf5
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2019-03-07 17:36:17 -0500

    spring cleaning

--- themes/blksail/static/js/main.js
+++ themes/blksail/static/js/main.js
@@ -6,18 +6,18 @@ function contact() {
   };
 
   /* Preparing the request with method, headers, url and data (body or payload)
- * Correct content type header is "application/json". 
- * To avoid CORS from Webbrowser-based repl.it please use "text/plain".
- */
-var request = {
-  url: "https://hooks.zapier.com/hooks/catch/439952/peam5v/silent/", // URL please use HTTPS
-  method: "post", // HTTP verb/method
-  headers: { // header information
-    "Accept": "application/json", // accept type (optional)
-    "Content-Type": "text/plain" // content type ("application/json"; "text/plain" to avoid CORS)
-  },
-  data: JSON.stringify(data) // stringify converts a JavaScript object into a string
-};
+  * Correct content type header is "application/json".
+  * To avoid CORS from Webbrowser-based repl.it please use "text/plain".
+  */
+  var request = {
+    url: "https://hooks.zapier.com/hooks/catch/439952/peam5v/silent/", // URL please use HTTPS
+    method: "post", // HTTP verb/method
+    headers: { // header information
+      "Accept": "application/json", // accept type (optional)
+      "Content-Type": "text/plain" // content type ("application/json"; "text/plain" to avoid CORS)
+    },
+    data: JSON.stringify(data) // stringify converts a JavaScript object into a string
+  };
 
   $.ajax(request).done(function (response) {
     console.log(response);
