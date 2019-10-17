c71ef1d6eba61e52f1545f7ce9048b37bc15c670
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2019-10-17 12:06:44 -0400

    volunteer form: switch from google forms to airtable

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -9,16 +9,16 @@ const translation = {
     'Good question! The DSA National Tech Committee (NTC) is a collection of volunteer tech-workers from across the country. We research and implement tech solutions to meet the various needs of DSA’s national leadership and local chapters, particularly with respect to collaboration and communication.',
   'Can the NTC help my chapter': 'Can the NTC help my chapter, committee, or working group with a project?',
   'Sure can':
-    'Sure can do! We love making tools for our comrades. Fill out our project help request form (link above) with the details of your situation and we will get back to you to discuss options.',
+    'Sure can do! We love making tools for our comrades. Fill out our project help request form (link above) with the details of your situation and we will get back to you to discuss options. Note that because we are all volunteers, we might not necessarily have the capacity to help with every project idea.',
   'My email is broken': 'My email, Slack, Wordpress, or Nintendo DS is broken... Can you help?',
   Nope:
-    'Nope! Unfortunately, the NTC does not currently accept requests for technical support. Maybe one day... but for now, we have limited resources and need to dedicate all our energy to building and maintaining digital infrastructure.',
+    'Unfortunately, the NTC does not currently accept requests for technical support. Maybe one day... but for now, we have limited resources and need to dedicate all our energy to building and maintaining digital infrastructure.',
   'Interested in tech': "I'm really interested in technology! How can I help?",
   'Welcome aboard':
     'Welcome aboard! Fill out our volunteer signup form to give us your contact info and skillset. We will get you onboarded into our communications and set you to work on a project ASAP!',
   'I found a problem': 'I found a bug on a DSA website or want to request a new feature. Can I reach out for support?',
   'Just fill out a ticket':
-    'Just fill out a ticket using the "Report an Issue" link above. Please note that we cannot make any promises to get around to fixing this issue quickly; we have limited capacity as a group and will try to review your request as soon as we can.',
+    'Just fill out a ticket using the "Help Form" link above. Please note that we cannot make any promises to get around to fixing this issue quickly; we have limited capacity as a group and will try to review your request as soon as we can.',
   'Volunteer Signup Form': 'Volunteer Signup Form',
   'Help Form': 'Help Form',
   'Report an Issue': 'Report an Issue',
--- src/paths.ts
+++ src/paths.ts
@@ -1,27 +1,4 @@
-import qs from 'querystring';
-
 const supportEmail = 'dsausatech@gmail.com';
-const gitlabEmail = 'incoming+dsausa-issue-tracker-11284343-issue-@incoming.gitlab.com';
-const emailParams = qs.stringify({
-  cc: supportEmail,
-  subject: 'Report an issue',
-  body: `
-Please note that we cannot make any promises to get around to fixing this issue quickly; we have limited capacity as
-a group and will try to review your request as soon as we can.
-
-Is this a feature request or bug report?
-
-ANSWER HERE
-
-What DSA website or application is this report for?
-
-ANSWER HERE
-
-Please provide details about your request below.
-
-ANSWER HERE
-  `.trim(),
-});
 
 const paths = {
   navbarLinks: [
@@ -32,14 +9,13 @@ const paths = {
     {
       text: 'Volunteer Signup Form',
       link:
-        'https://docs.google.com/forms/d/e/1FAIpQLSeDuRP62PN2yisw-4r513dYCZJFaqRKBcQjcV7JR0hjepiSzg/viewform',
+        'https://airtable.com/shrsadbAsPqiyfXGp',
     },
     {
       text: 'Help Form',
       link:
         'https://docs.google.com/forms/d/e/1FAIpQLSejt71aXXUjH0l0Nj1SnvV1KuhJ66N9Dfih4EiLu5W151H_Uw/viewform',
     },
-    { text: 'Report an Issue', link: `mailto:${gitlabEmail}?${emailParams}` },
     { text: 'Email Us', link: `mailto:${supportEmail}` },
   ],
 };
